4617d24c901b5291c5330191875fa22b7c39234b
“Ori <chandleretienne@gmail.com>
2017-01-05 01:21:25 -0600

    Initial commit

---
+++ app/src/main/java/com/tricloudcommunications/ce/instagramclone/MainActivity.java
@@ -0,0 +1,79 @@
+package com.tricloudcommunications.ce.instagramclone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParsePushBroadcastReceiver;
+import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
+import com.parse.SignUpCallback;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        //Check if the user is already logged in.
+        if (ParseUser.getCurrentUser() != null){
+
+            Log.i("curentUser", "User is logged in " + ParseUser.getCurrentUser().getUsername());
+
+        }else {
+
+            Log.i("curentUser", "User is Not logged in ");
+
+        }
+
+        //Initialize Parse Push notifications
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        //Initialize Parse
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/tricloudcommunications/ce/instagramclone/StarterApplication.java
@@ -0,0 +1,62 @@
+package com.tricloudcommunications.ce.instagramclone;
+
+/**
+ * Created by CE on 1/4/2017.
+ */
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("udemyInstagramApp")
+                .clientKey("mila12130906")
+                .server("https://ovh.tricloudcommunications.com:1338/parseInstagram/")
+                .build()
+        );
+
+        /**
+         ParseObject object = new ParseObject("ExampleObject");
+         object.put("myNumber", "123");
+         object.put("myString", "rob");
+         object.saveInBackground(new SaveCallback () {
+        @Override
+        public void done(ParseException ex) {
+        if (ex == null) {
+        Log.i("Parse Result", "Successful!");
+        } else {
+        Log.i("Parse Result", "Failed" + ex.toString());
+        }
+        }
+        });
+         */
+
+
+        //ParseUser.enableAutomaticUser(); //Used if you want the app to automatically create a new user. In most cases we do not, so we will comment this out.
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+
+
+}
